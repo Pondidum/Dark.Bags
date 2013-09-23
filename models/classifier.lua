fb1cc211b8e9c9bdf9f6717a17c6dabd02ea9f93
andy <andy@stormbase.net>
2013-09-24 00:31:29 +0100

    classifier: add into namespace

--- models/classifier.lua
+++ models/classifier.lua
@@ -2,33 +2,58 @@ local addon, ns = ...
 
 local core = Dark.core
 
-local classifier = {
-	
-	new = function() 
+local classifiers = {}
 
-		local this = setmetatable({}, { __index = self })
+ns.classifiers = {
+    
+	new = function(self) 
+
+		local this = {}
 
 		this.events = core.events.new()
 		
+		this.addRescanEvent = function(self, event)
+			self.events.register(event, self:requestRescan)
+		end
+
+		this.requestRescan = function(self) end
+		this.beforeClassify = function(self) end
+		this.classify = function(self, details) end
+		this.afterClassify = function(self) end
+
+		self:add(this)
+
 		return this
 
 	end,
 
-	addRescanEvent = function(self, event)
-		self.events.register(event, self:requestRescan)
-	end,
+	add = function(self, classifier)
 
-	requestRescan = function(self)
-	
+		table.insert(classifiers, classifier)
+
 	end,
 
 	beforeClassify = function(self)
-	end
 
-	classify = function(self, details)
+		for i, c in ipairs(classifiers) do
+			c:beforeClassify()
+		end
+
+	end,
+
+	classify = function(self, detail)
+	
+		for i, c in ipairs(classifier) do
+			c:classify(detail)
+		end
+
 	end,
 
 	afterClassify = function(self)
-	end,
 
+		for i, c in ipairs(classifiers) do
+			c:afterClassify()
+		end
+
+	end
 }
