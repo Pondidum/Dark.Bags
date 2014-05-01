6b2084697bf3b29ab75f976fd9a64667a425256b
andy <andy@andydote.co.uk>
2014-05-01 23:38:23 +0100

    views.item: use ContainerFrameItemButtonTemplate for base

--- views/item.lua
+++ views/item.lua
@@ -4,15 +4,21 @@ local core = Dark.core
 local style = core.style
 
 ns.views.item = {
-	
+
 	new = function(name)
-		
-		local this = CreateFrame("CheckButton", name, nil, "SecureActionButtonTemplate, SecureHandlerDragTemplate")
+
+		local this = CreateFrame("CheckButton", name, nil, "ContainerFrameItemButtonTemplate")
+
+		this:SetPushedTexture("")
+		this:SetNormalTexture("")
+		_G[this:GetName() .."NewItemTexture"]:Hide()
+
+		this:ClearAllPoints()
 		this:SetSize(24, 24)
-		
+
 		style.addShadow(this)
 
-		local icon = this:CreateTexture(nil, "HIGH")
+		local icon = this.icon
 		icon:SetAllPoints(this)
 		icon:SetTexCoord(.08, .92, .08, .92)
 
@@ -20,22 +26,8 @@ ns.views.item = {
 			icon:SetTexture(details.texture)
 
 			this:SetID(details.slot)
+			this:Show()
 		end
-
-		this:RegisterForDrag("LeftButton", "RightButton")
-		this:RegisterForClicks("AnyUp")
-
-		this:SetScript("OnClick", function(self, button)
-			ContainerFrameItemButton_OnClick(this, button)
-		end)
-
-		this:SetScript("OnDragStart", function(self, button) 
-			ContainerFrameItemButton_OnClick(this, "LeftButton")
-		end)
-
-		this:SetScript("OnReceiveDrag", function(self) 
-			ContainerFrameItemButton_OnClick(this, "LeftButton")
-		end) 
 
 		return this
 
