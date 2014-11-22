a975e6fcc0b6979cc51fcc21ad8fc3e3c62bc609
andy <andy@andydote.co.uk>
2014-11-22 19:23:43 +0000

    currencyDisplay: display all currencies from config

--- views/currencyDisplay.lua
+++ views/currencyDisplay.lua
@@ -7,7 +7,6 @@ local ui = core.ui
 
 local config = ns.config
 
-
 local currencyItem = {
 
 	new = function(self, ...)
@@ -20,7 +19,6 @@ local currencyItem = {
 	end,
 
 	ctor = function(self, currencyID)
-
 
 		local frame = CreateFrame("Frame", "$parentItem"..currencyID , UIParent)
 		frame:SetSize(50, 15)
@@ -63,12 +61,28 @@ local currencyDisplay = {
 		style.applyBackgroundTo(frame)
 		style.addShadow(frame)
 
-		local item = currencyItem:new(823)
-		item.frame:SetParent(frame)
-		item.frame:SetPoint("LEFT", frame, "LEFT", 0, 0)
+		local prev
+		local updates = {}
+
+		for i, currencyID in ipairs(config.currencies) do
+
+			local item = currencyItem:new(currencyID)
+			item.frame:SetParent(frame)
+
+			if prev then
+				item.frame:SetPoint("LEFT", prev, "RIGHT", config.spacing, 0)
+			else
+				item.frame:SetPoint("LEFT", frame, "LEFT", 0, 0)
+			end
+
+			prev = item.frame
+			table.insert(updates, function() item:update() end)
+		end
 
 		local updateAll = function()
-			item:update()
+			for i, update in ipairs(updates) do
+				update()
+			end
 		end
 
 		events.register("CURRENCY_DISPLAY_UPDATE", updateAll)
