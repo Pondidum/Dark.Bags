90e0357b9ce58861d81e6c274460435a9da4f44e
andy <andy@andydote.co.uk>
2014-11-22 19:04:24 +0000

    currencyDisplay: abstract currencyItem

--- views/currencyDisplay.lua
+++ views/currencyDisplay.lua
@@ -7,6 +7,51 @@ local ui = core.ui
 
 local config = ns.config
 
+
+local currencyItem = {
+
+	new = function(self, ...)
+
+		local this = setmetatable({}, { __index = self })
+		this:ctor(...)
+
+		return this
+
+	end,
+
+	ctor = function(self, currencyID)
+
+
+		local frame = CreateFrame("Frame", "$parentItem"..currencyID , UIParent)
+		frame:SetSize(50, 15)
+
+		local icon = frame:CreateTexture()
+		icon:SetPoint("LEFT", 1, 0)
+		icon:SetSize(15, 15)
+
+		local label = ui.createFont(frame)
+		label:SetPoint("LEFT", icon, "RIGHT", 1, 0)
+		label:SetPoint("RIGHT")
+		label:SetHeight(15)
+
+		self.currencyID = currencyID
+		self.frame = frame
+		self.icon = icon
+		self.label = label
+
+	end,
+
+	update = function(self)
+
+		local name, amount, texturePath, earnedThisWeek, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(self.currencyID)
+
+		self.icon:SetTexture(texturePath)
+		self.label:SetText(amount)
+
+	end
+}
+
+
 local currencyDisplay = {
 	new = function()
 
@@ -18,25 +63,17 @@ local currencyDisplay = {
 		style.applyBackgroundTo(frame)
 		style.addShadow(frame)
 
-		local icon = frame:CreateTexture()
-		icon:SetPoint("LEFT", 1, 0)
-		icon:SetSize(15, 15)
+		local item = currencyItem:new(823)
+		item.frame:SetParent(frame)
+		item.frame:SetPoint("LEFT", frame, "LEFT", 0, 0)
 
-		local label = ui.createFont(frame)
-		label:SetPoint("LEFT", icon, "RIGHT", 1, 0)
-		label:SetPoint("RIGHT")
-		label:SetHeight(15)
-
-		local update = function()
-			local name, amount, texturePath, earnedThisWeek, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(823)
-
-			icon:SetTexture(texturePath)
-			label:SetText(amount)
+		local updateAll = function()
+			item:update()
 		end
 
-		events.register("CURRENCY_DISPLAY_UPDATE", update)
+		events.register("CURRENCY_DISPLAY_UPDATE", updateAll)
 
-		update()
+		updateAll()
 
 	end
 }
