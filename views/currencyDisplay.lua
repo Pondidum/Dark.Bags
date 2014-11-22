c241eb3f15679837d71524983ff1e419ba90d4c4
andy <andy@andydote.co.uk>
2014-11-22 18:46:18 +0000

    currencyDisplay: spaces=>tabs

--- views/currencyDisplay.lua
+++ views/currencyDisplay.lua
@@ -8,37 +8,37 @@ local ui = core.ui
 local config = ns.config
 
 local currencyDisplay = {
-  new = function()
+	new = function()
 
-    local frame = CreateFrame("Frame", "DarkBagsCurrency", UIParent)
-    frame:SetPoint("BOTTOMLEFT", DarkBagsGold, "TOPLEFT", 0, 4)
-    frame:SetPoint("BOTTOMRIGHT", DarkBagsGold, "TOPRIGHT", 0, 4)
-    frame:SetHeight(15)
+		local frame = CreateFrame("Frame", "DarkBagsCurrency", UIParent)
+		frame:SetPoint("BOTTOMLEFT", DarkBagsGold, "TOPLEFT", 0, 4)
+		frame:SetPoint("BOTTOMRIGHT", DarkBagsGold, "TOPRIGHT", 0, 4)
+		frame:SetHeight(15)
 
-    style.applyBackgroundTo(frame)
-    style.addShadow(frame)
+		style.applyBackgroundTo(frame)
+		style.addShadow(frame)
 
-    local icon = frame:CreateTexture()
-    icon:SetPoint("LEFT", 1, 0)
-    icon:SetSize(15, 15)
+		local icon = frame:CreateTexture()
+		icon:SetPoint("LEFT", 1, 0)
+		icon:SetSize(15, 15)
 
-    local label = ui.createFont(frame)
-    label:SetPoint("LEFT", icon, "RIGHT", 1, 0)
-    label:SetPoint("RIGHT")
-    label:SetHeight(15)
+		local label = ui.createFont(frame)
+		label:SetPoint("LEFT", icon, "RIGHT", 1, 0)
+		label:SetPoint("RIGHT")
+		label:SetHeight(15)
 
-    local update = function()
-      local name, amount, texturePath, earnedThisWeek, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(823)
+		local update = function()
+			local name, amount, texturePath, earnedThisWeek, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(823)
 
-      icon:SetTexture(texturePath)
-      label:SetText(amount)
-    end
+			icon:SetTexture(texturePath)
+			label:SetText(amount)
+		end
 
-    events.register("CURRENCY_DISPLAY_UPDATE", update)
+		events.register("CURRENCY_DISPLAY_UPDATE", update)
 
-    update()
+		update()
 
-  end
+	end
 }
 
 ns.currencyDisplay = currencyDisplay
