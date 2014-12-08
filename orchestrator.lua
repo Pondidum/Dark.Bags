673415fd364121e9c236f39f6ba08481ab642a5e
andy <andy@andydote.co.uk>
2014-12-08 22:45:16 +0000

    orchestrator: update bag model from events

---
+++ orchestrator.lua
@@ -0,0 +1,33 @@
+local addon, ns = ...
+
+local class = Darker.class
+local events = Darker.events
+
+local orchestrator = class:extend({
+
+	ctor = function(self, slotBuilder)
+		self:include(events)
+
+		self:register("BAG_UPDATE_COOLDOWN")
+		self:register("BAG_UPDATE_DELAYED")
+
+		self.builder = slotBuilder
+	end,
+
+	BAG_UPDATE_DELAYED = function(self)
+		self:updateSlots()
+	end,
+
+	BAG_UPDATE_COOLDOWN = function(self)
+		self:updateSlots()
+	end,
+
+	updateSlots = function(self)
+
+		for i, slot in ipairs(self.builder.slots) do
+			slot:updateModel()
+			slot:updateView()
+		end
+
+	end,
+})
