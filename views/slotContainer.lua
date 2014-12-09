771497053b9b01a0782ce13629a93b1512368e90
andy <andy@andydote.co.uk>
2014-12-09 18:01:21 +0000

    views: add a slotContainer (generic holder for many slotComponents)

---
+++ views/slotContainer.lua
@@ -0,0 +1,29 @@
+local addon, ns = ...
+
+local class = ns.lib.class
+local layoutEngine = ns.lib.engine
+
+local slotContainer = class:extend({
+
+	ctor = function(self, nameSuffix)
+
+		self.frame = CreateFrame("Frame", "DarkBagsContainer" .. nameSuffix, UIParent)
+		self.frame:SetWidth(250)
+
+		self.engine = layoutEngine:new(self.frame, {
+			type = "horizontal",
+			origin = "TOPLEFT",
+			wrap = true,
+			itemSpacing = 6,
+			autosize = "y"
+		})
+
+	end,
+
+	add = function(self, component)
+		self.engine:addChild(component)
+	end,
+
+})
+
+ns.slotContainer = slotContainer
