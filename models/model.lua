64172024c032277e66647136b762b45d8387482f
andy <andy@andydote.co.uk>
2015-01-30 21:21:07 +0000

    model: remove commented code

--- models/model.lua
+++ models/model.lua
@@ -54,8 +54,6 @@ local model = {
 		events.register("BAG_UPDATE_COOLDOWN", onCooldownsUpdated)
 		events.register("ITEM_LOCK_CHANGED", onCooldownsUpdated)
 
-		--classifiers.onRescanRequested(fullRescan)
-
 		this.onContentsChanged = function()
 		end
 
